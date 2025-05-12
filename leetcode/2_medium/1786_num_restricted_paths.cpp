0d7fc5721cfdcd4d4aa21b469f60d7414c060f96
fancywhale23 <justintaoau@gmail.com>
2025-05-12 11:42:50 +1000

    fixed directory and fix to 1786

--- leetcode/2_medium/1786_num_restricted_paths.cpp
+++ leetcode/2_medium/1786_num_restricted_paths.cpp
@@ -1,31 +1,30 @@
 #include <climits>
 #include <functional>
-#include <iostream>
 #include <queue>
 #include <utility>
 #include <vector>
 class Solution {
   public:
-    long mod = 1e9 + 7;
-    long countPaths(std::vector<std::vector<std::pair<int, int>>> &graph,
-                    std::vector<long> dp, std::vector<int> &dist, int src) {
+    int mod = 1e9 + 7;
+
+    int countPaths(std::vector<std::vector<std::pair<int, int>>> &graph,
+                   std::vector<int> &dp, std::vector<int> &dist, int src) {
         if (src == 1)
             return 1;
 
-        if (dp[src] != -1) {
+        if (dp[src] != -1)
             return dp[src];
-        }
 
-        long count = 0;
+        int count = 0;
         for (auto &n : graph[src]) {
             if (dist[n.first] > dist[src]) {
-                count =
-                    (count % mod + countPaths(graph, dp, dist, n.first) % mod);
+                count = (count + countPaths(graph, dp, dist, n.first)) % mod;
             }
         }
 
-        return dp[src] = count % mod;
+        return dp[src] = count;
     }
+
     int countRestrictedPaths(int n, std::vector<std::vector<int>> &edges) {
         std::vector<std::vector<std::pair<int, int>>> graph(
             n + 1, std::vector<std::pair<int, int>>());
@@ -34,7 +33,6 @@ class Solution {
             graph[edge[0]].push_back({edge[1], edge[2]});
             graph[edge[1]].push_back({edge[0], edge[2]});
         }
-
         // Dijkstras
         std::vector<int> dist(n + 1, INT_MAX);
         std::priority_queue<std::vector<int>, std::vector<std::vector<int>>,
@@ -55,18 +53,7 @@ class Solution {
             }
         }
 
-        for (int i = 1; i < graph.size(); i++) {
-            std::cout << i << " -> ";
-            for (auto n : graph[i]) {
-                std::cout << n.first << ',' << n.second << ' ';
-            }
-
-            std::cout << '\n';
-        }
-
-        // Count paths from 1->n in DAG
-        std::vector<long> dp(n + 1, -1);
-
+        std::vector<int> dp(n + 1, -1);
         return countPaths(graph, dp, dist, n);
     }
 };
