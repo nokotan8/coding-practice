370ee0775b43fffeb315803193f5e1d9e03b704e
fancywhale23 <justintaoau@gmail.com>
2025-05-30 11:42:57 +1000

    leetcode 1046

---
+++ leetcode/1_easy/1046_last_stone_weight.cpp
@@ -0,0 +1,18 @@
+#include <queue>
+#include <vector>
+class Solution {
+  public:
+    int lastStoneWeight(std::vector<int> &stones) {
+        std::priority_queue<int> pq(stones.begin(), stones.end());
+        while (pq.size() > 1) {
+            int y = pq.top();
+            pq.pop();
+            int x = pq.top();
+            pq.pop();
+            if (x != y)
+                pq.push(y - x);
+        }
+
+        return pq.size() ? pq.top() : 0;
+    }
+};
