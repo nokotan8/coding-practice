81c284eed8782243f713c8cb1e162144ed5c914b
nokotan8 <justintaoau@gmail.com>
2025-06-22 12:12:16 +1000

    leetcode 322 (added soln)

--- leetcode/2_medium/0322_coin_change.cpp
+++ leetcode/2_medium/0322_coin_change.cpp
@@ -4,29 +4,52 @@
 
 class Solution {
   public:
-    int calc(std::vector<int> &coins, int amount, std::vector<int> &dp) {
-        if (dp[amount] != -1)
-            return dp[amount];
+    int coinChange(std::vector<int> &coins, int amount) {
+        std::vector<int> dp(amount + 1, INT_MAX);
+        dp[0] = 0;
 
-        int res = INT_MAX;
-        for (size_t i = 0; i < coins.size(); i++) {
-            if (amount >= coins[i]) {
-                int num = calc(coins, amount - coins[i], dp);
-                if (num != INT_MAX)
-                    res = std::min(res, num + 1);
+        for (int curr = 1; curr <= amount; curr++) {
+            for (int coin : coins) {
+                if (curr >= coin && dp[curr - coin] != INT_MAX) {
+                    dp[curr] = std::min(dp[curr], 1 + dp[curr - coin]);
+                }
             }
         }
 
-        dp[amount] = res;
-        return dp[amount];
-    }
-
-    int coinChange(std::vector<int> &coins, int amount) {
-        std::vector<int> dp(amount + 1, -1);
-        dp[0] = 0;
-
-        int res = calc(coins, amount, dp);
-
-        return res == INT_MAX ? -1 : res;
+        return dp[amount] == INT_MAX ? -1 : dp[amount];
     }
 };
+
+// #include <algorithm>
+// #include <climits>
+// #include <vector>
+//
+// class Solution {
+//   public:
+//     int calc(std::vector<int> &coins, int amount, std::vector<int> &dp) {
+//         if (dp[amount] != -1)
+//             return dp[amount];
+//
+//         int res = INT_MAX;
+//         for (size_t i = 0; i < coins.size(); i++) {
+//             if (amount >= coins[i]) {
+//                 int num = calc(coins, amount - coins[i], dp);
+//                 if (num != INT_MAX)
+//                     res = std::min(res, num + 1);
+//             }
+//         }
+//
+//         dp[amount] = res;
+//         return dp[amount];
+//     }
+//
+//     int coinChange(std::vector<int> &coins, int amount) {
+//         std::vector<int> dp(amount + 1, -1);
+//         dp[0] = 0;
+//
+//         int res = calc(coins, amount, dp);
+//
+//         return res == INT_MAX ? -1 : res;
+//     }
+// };
+//
