d8f24aff193bf2eeb527ca9c9d67102b50605175
nokotan8 <justintaoau@gmail.com>
2025-08-19 14:24:20 +1000

    leetcode 869 optimised

--- leetcode/2_medium/0869_reordered_power_of_2.cpp
+++ leetcode/2_medium/0869_reordered_power_of_2.cpp
@@ -1,50 +1,22 @@
+#include <algorithm>
 #include <string>
-#include <unordered_map>
 #include <unordered_set>
 
 class Solution {
-  private:
-    std::unordered_set<int> powers;
-    int len;
-
-    bool backtrack(std::string &curr, std::unordered_map<char, int> &freq,
-                   int rem) {
-        if (rem == 0) {
-            return powers.count(std::stoi(curr)) == 1;
-        }
-
-        for (auto &[c, f] : freq) {
-            if (f > 0 && ((len == rem && c != '0') || rem != len)) {
-                f--;
-                curr.push_back(c);
-
-                if (backtrack(curr, freq, rem - 1)) {
-                    return true;
-                }
-
-                curr.pop_back();
-                f++;
-            }
-        }
-
-        return false;
-    }
-
   public:
     bool reorderedPowerOf2(int n) {
+        std::unordered_set<std::string> powers;
+
         for (int i = 0, p = 1; i < 30; i++, p *= 2) {
-            powers.insert(p);
+            std::string p_str = std::to_string(p);
+            std::sort(p_str.begin(), p_str.end());
+
+            powers.insert(p_str);
         }
 
-        std::unordered_map<char, int> freq;
         std::string n_str = std::to_string(n);
-        len = n_str.size();
+        std::sort(n_str.begin(), n_str.end());
 
-        for (char c : n_str) {
-            freq[c]++;
-        }
-
-        std::string curr = "";
-        return backtrack(curr, freq, n_str.size());
+        return powers.count(n_str) == 1;
     }
 };
