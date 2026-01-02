f7434e871cba74afdc95aff249c68e5af063f733
nokotan8 <justintaoau@gmail.com>
2026-01-02 14:58:41 +1100

    leetcode 22

--- leetcode/2_medium/0022_generate_parens.cpp
+++ leetcode/2_medium/0022_generate_parens.cpp
@@ -1,30 +1,37 @@
-// https://leetcode.com/problems/generate-parentheses
 #include <string>
 #include <vector>
 
-using std::string;
 class Solution {
+  private:
+    std::vector<std::string> res;
+    int n;
+
+    void recurse(std::string &s, int ex, int total) {
+        if (s.size() == n * 2) {
+            res.push_back(s);
+            return;
+        }
+
+        // Total '(' not reached max.
+        if (total < n) {
+            s.push_back('(');
+            recurse(s, ex + 1, total + 1);
+            s.pop_back();
+        }
+        // Excess forward parens.
+        if (ex > 0) {
+            s.push_back(')');
+            recurse(s, ex - 1, total);
+            s.pop_back();
+        }
+    }
+
   public:
-    std::vector<string> res;
-    std::vector<string> generateParenthesis(int n) {
-        std::vector<int> parens = {0, 0};
-        generate("", n, parens);
+    std::vector<std::string> generateParenthesis(int _n) {
+        n = _n;
+        std::string s;
+        recurse(s, 0, 0);
 
         return res;
-    }
-    void generate(string curr, int n, std::vector<int> parens) {
-        if (parens[0] == n && parens[1] == n) {
-            res.push_back(curr);
-        } else {
-            if (parens[0] < n) {
-                parens[0]++;
-                generate(curr + '(', n, parens);
-                parens[0]--;
-            }
-            if (parens[0] > parens[1]) {
-                parens[1]++;
-                generate(curr + ')', n, parens);
-            }
-        }
     }
 };
